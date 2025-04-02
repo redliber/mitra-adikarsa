e9f584789fbc22c30b973f394816159474ecc8c0
Ben <redliber@tutamail.com>
2025-04-02 16:47:12 +0700

    Adding Base in Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://redliber.github.io'
+    site: 'https://redliber.github.io',
+    base: 'mitra-adikarsa'
 });
