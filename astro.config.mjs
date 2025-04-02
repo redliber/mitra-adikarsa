c8d4e04f3c47b7212d43b9e42d24bb995b368d01
Ben <redliber@tutamail.com>
2025-04-02 16:50:05 +0700

    Added a slash for base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://redliber.github.io',
-    base: 'mitra-adikarsa'
+    base: '/mitra-adikarsa'
 });
