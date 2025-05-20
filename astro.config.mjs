d53e42dafacae30773fa5d7261771e4d11701d6b
Bernardus Bernard <redliber@tutamail.com>
2025-05-20 14:46:30 +0700

    Adding CNAME and defineConfig custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://redliber.github.io',
-  base: '/mitra-adikarsa',
+  site: 'https://mitraadikarsa.com',
+  // base: '/mitra-adikarsa',
   integrations: [react()],
 
   vite: {
